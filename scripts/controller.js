054a6ffae458c3719ca59a3f454b36842df061fd
LaJaunieS <LaJaunieS@users.noreply.github.com>
2017-02-15 15:48:48 -0800

    refactoring functions to receive arguments from inputs

--- scripts/controller.js
+++ scripts/controller.js
@@ -12,6 +12,28 @@ angular.module("ptabApp",[])
                 queryErrorMessage: ""
             };
             
+            $scope.patentNumberJson = {}
+            $scope.patentSearch = "trials?patentNumber="; //builds url string for patent search
+            
+            function getDocuments(url){
+                    evt.preventDefault();
+                    let documentsCall = {
+                        method: "GET",
+                        url: url
+                    };
+                    //load document list template(TBD)
+                };
+            
+            
+             $scope.patentSearchCall =  {
+                    method: 'GET',
+                    url: "https://ptabdata.uspto.gov/ptab-api/" + $scope.patentSearch    + $scope.query.patentNumber,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+            
+                        
             
             
             //make api call based on query string parameters
@@ -43,22 +65,47 @@ angular.module("ptabApp",[])
             
             
             //functions to make calls to api
-            $scope.apiCall = function() {
-                let patentSearchCall =  {
-                    method: 'GET',
-                    url: "https://ptabdata.uspto.gov/ptab-api/trials?patentNumber=" + $scope.query.patentNumber,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
+            $scope.apiCall = function(call) {
+                //call argument passed in from getPatentData() on click       
+                //console.log(call);
+//                $scope.caseSearchCall = {
+//                   method: 'GET',
+//                    url: "https://ptabdata.uspto.gov/ptab-api/trials/" + $scope.query.caseNumber + "/documents",
+//                    headers: {
+//                        'Content-Type': 'application/json'
+//                    }                    
+//                };
+                //call argument passed in from apiCall() -->getPatentData()
+                $http(call).then(
+                    function successCallback(data) {
+                    $scope.patentNumberJson = data;
+                    queryErrorExists =  $scope.patentNumberJson.data.results.length == 0;                      
+                        if (queryErrorExists) {
+                        $scope.query.queryErrorMessage = "That patent number doesn't exist, is not in the PTAB database or there was an error retrieving your query. Please enter another number or try your request again";
+                        console.log($scope.query.queryErrorMessage);
+                        //add error template to DOM
+                    } else {
+                        console.log($scope.patentNumberJson);
+                        //$scope.addTemplatesToDom('current-conditions', 'ConditionsTemplateUrl');
+                    };
+                        
+                    },
+                    function errorCallback() {
+                            alert('Failed to connect to the API. Please check your connection and try again.');
+                            });
                 };
+            
+             
+        //aggregate functions necessary to build url string, make api call, and load templates
+            $scope.getPatentData = function(call, search_type, search_param) {
+                call["url"] ="https://ptabdata.uspto.gov/ptab-api/" + search_type + search_param;
+                console.log(call.url)
+                $scope.apiCall(call);   
+                };
+            
+            
+        }]) //close controller
                 
-                let caseSearchCall = {
-                   method: 'GET',
-                    url: "https://ptabdata.uspto.gov/ptab-api/trials/" + $scope.query.caseNumber + "/documents",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }                    
-                };
                 
                 
 //                $http(caseSearchCall).then(
@@ -82,32 +129,7 @@ angular.module("ptabApp",[])
                 
                 
                 
-                $http(patentSearchCall).then(
-                    function successCallback(data) {
-                    $scope.patentNumberJson = data;
-                    queryErrorExists =  $scope.patentNumberJson.data.results.length == 0;                      
-                        if (queryErrorExists) {
-                        $scope.query.queryErrorMessage = "That patent number doesn't exist or there was an error retrieving your query. Please enter another number or try your request again";
-                        console.log($scope.query.queryErrorMessage);
-                        //add error template to DOM
-                    } else {
-                        console.log($scope.patentNumberJson);
-                        //$scope.addTemplatesToDom('current-conditions', 'ConditionsTemplateUrl');
-                    };
-                        
-                    },
-                    function errorCallback() {
-                            alert('Failed to connect to the API. Please check your connection and try again.');
-                            });
-                };
-            
-             
-        //aggregate functions necessary to build url string, make api call, and load templates
-            $scope.getPatentData = function() {
-                console.log($scope.query.patentNumber)
-                $scope.apiCall();   
-                };
-        }]) //close controller
+                
         
         
 
