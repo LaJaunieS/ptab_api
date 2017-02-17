bb4e5ef01ad5dd28a2f7562f03ea3d63f9d56522
LaJaunieS <LaJaunieS@users.noreply.github.com>
2017-02-17 11:09:18 -0800

    Continue error handling, updating comments, cleanup

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,12 +1,12 @@
-document.getElementById("case-search-results").style.display = "none";
-document.getElementById("patent-search-results").style.display = "none";
-document.getElementById("error-message").style.display = "none";
-document.getElementById("patent-search").style.display = "none";
-document.getElementById("case-search").style.display = "none";
-
-
-
-             
+        //default string for case search=  https://ptabdata.uspto.gov/ptab-api/trials/ + {trialNo} + /documents? + title={doc title}
+            
+            //default string for patent search = https://ptabdata.uspto.gov/ptab-api/trials?patentNumber= + {patentNo}
+            
+            // patent search https://ptabdata.uspto.gov/ptab-api/trials?patentNumber=6314409 -- returns a clickable list of cases
+            
+            // case search https://ptabdata.uspto.gov/ptab-api/trials/CBM2014-00157/documents   -- returns a clickable list of documents
+            
+             //doc search in a case= https://ptabdata.uspto.gov/ptab-api/trials/ipr2014-00786/documents?title=petition
 
 
 angular.module("ptabApp",[])
@@ -15,9 +15,7 @@ angular.module("ptabApp",[])
         .controller('ptabCtrl', ['$scope', '$http', function($scope, $http){
             
             console.log('angular app loaded');
-            
-            
-            
+                        
             //build query string based on user input
             $scope.query = {
                 patentNumber: "",
@@ -28,11 +26,13 @@ angular.module("ptabApp",[])
             
             $scope.ps = document.getElementById("patent-search");
             $scope.cs = document.getElementById("case-search");
-            $scope.patentNumberJson = {}
+            
+            $scope.patentNumberJson = {}; //will hold json data
             $scope.patentSearch = "trials?patentNumber="; //builds url string for patent search
             $scope.caseSearch = "trials/" + $scope.query.caseNumber;
             $scope.caseTitleSearch = "trials/" + $scope.query.caseNumber;
             
+            //get request to download a single document
             function getDocuments(url){
                     evt.preventDefault();
                     let documentsCall = {
@@ -41,9 +41,7 @@ angular.module("ptabApp",[])
                     };
                     //load document list template(TBD)
                 };
-            
-                
-            
+                        
              $scope.patentSearchCall =  {
                     method: 'GET',
                     url: "https://ptabdata.uspto.gov/ptab-api/" + $scope.patentSearch    + $scope.query.patentNumber,
@@ -67,10 +65,8 @@ angular.module("ptabApp",[])
                         'Content-Type': 'application/json'
                     }                    
                 };
-            
-            
-            
-             
+                        
+            //bringing templates in and out of view 
             $scope.addTemplateToDom = function(el) {
                 document.getElementById(el).style.display = "initial";
             };
@@ -87,50 +83,17 @@ angular.module("ptabApp",[])
                 $scope.removeTemplateFromDom("case-search-results");
             };
               
-            
-                        
-            
-            
-            //make api call based on query string parameters
-            
-            //default string for case search=  https://ptabdata.uspto.gov/ptab-api/trials/ + {trialNo} + /documents? + title={doc title}
-            
-            //default string for patent search = https://ptabdata.uspto.gov/ptab-api/trials?patentNumber= + {patentNo}
-            
-            //example request url 
-               
-            
-            // patent search https://ptabdata.uspto.gov/ptab-api/trials?patentNumber=6314409 -- returns a clickable list of cases
-            
-            // case search https://ptabdata.uspto.gov/ptab-api/trials/CBM2014-00157/documents   -- returns a clickable list of documents
-            
-             //doc search in a case= https://ptabdata.uspto.gov/ptab-api/trials/ipr2014-00786/documents?title=petition
-            
-            //hold response json in an object
-            
-                    
-            //parse object to display returned values to user
-            
-                    
-            //for patent search, click event to send another get request for case information
-            
-            
-            
-            
-            
-            
             //functions to make calls to api
             $scope.apiCall = function(call) {
-                //call argument passed in from getPatentData() on click       
-                
-                //call argument passed in from apiCall() -->getPatentData()
+            //call argument passed in from getPatentData() -->apiCall() --> $http()
                 $http(call).then(
                     function successCallback(data) {
                     $scope.jsonData = data;
                     
-                    if (call == $scope.patentSearchCall){
+                    if (call === $scope.patentSearchCall){
                         queryErrorExists =  $scope.jsonData.data.results.length === 0; 
                         if (queryErrorExists) {
+                            console.log(data);
                             $scope.removeTemplateFromDom("case-search-results");
                             $scope.removeTemplateFromDom("patent-search-results");
                             $scope.addTemplateToDom("error-message");
@@ -139,35 +102,42 @@ angular.module("ptabApp",[])
                             //add error template to DOM
                             } else {
                             console.log("Patent Search initiated");
+                            console.log($scope.jsonData);
                             $scope.removeTemplateFromDom("case-search-results");
                             $scope.removeTemplateFromDom("error-message");
                             $scope.addTemplateToDom("patent-search-results");
-                            console.log($scope.jsonData);
-                            //$scope.addTemplatesToDom('current-conditions', 'ConditionsTemplateUrl');
                             };
-                    } else if (call == $scope.caseSearchCall) {
+                    } else if (call === $scope.caseSearchCall) {
                         console.log("Case Search initiated");
+                        console.log($scope.jsonData);
                         $scope.removeTemplateFromDom("patent-search-results");
                         $scope.removeTemplateFromDom("error-message");
                         $scope.addTemplateToDom("case-search-results");
-                        console.log($scope.jsonData);
-                    } else if (call == $scope.caseTitleSearchCall) {
+                    } else if (call === $scope.caseTitleSearchCall) {
                         $scope.caseTitleData = $scope.jsonData; 
                         $scope.removeTemplateFromDom("patent-search-results");
                         $scope.removeTemplateFromDom("error-message");
                         $scope.addTemplateToDom("case-search-results");
-                    }
-                },
-                function errorCallback() {
-                        alert("The information entered was not a proper case number, or the app failed to connect to the API. Please ensure you entered data in the correct format. Or, please check your connection and try again.");
-                        });
                     };
+                },
+                function errorCallback(data) {
+                        //if 400 bad request
+                        //if 
+                        if(data.data.status === 400) {
+                            $scope.addTemplateToDom("error-message");
+                            $scope.query.queryErrorMessage = data.data.message;
+                            } else {
+                                $scope.addTemplateToDom("error-message");
+                                $scope.query.queryErrorMessage = "There was a problem connecting to the API. Please check your connection and try again.";
+                            };
+                        });  //close $http()
+                    };  //close apiCall() 
             
              
-        //aggregate functions necessary to build url string, make api call, and load templates
+        //aggregate function to build url string, make api call, and load templates
             $scope.getPatentData = function(call, search_type, search_param) {
                 call["url"] ="https://ptabdata.uspto.gov/ptab-api/" + search_type + search_param;
-                console.log(call.url)
+                console.log(call.url);
                 $scope.apiCall(call);   
                 };
             
